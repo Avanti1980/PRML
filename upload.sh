9f52a73e788ab9e1351d2f2f8f7913f854998dd7
Avanti <murongxixi@gmail.com>
2020-12-22 12:40:08 +0800

    update

--- upload.sh
+++ upload.sh
@@ -1,1 +1,3 @@
-git add 勘误.pdf 模式识别与机器学习.pdf 习题解答.pdf "Pattern Recognition And Machine Learning - Christopher M. Bishop - solution.pdf" "Pattern Recognition And Machine Learning - Christopher M. Bishop.pdf" solution.tex upload.sh
+git add 勘误.pdf 模式识别与机器学习.pdf 习题解答.pdf README.md solutions.tex upload.sh "Pattern Recognition And Machine Learning - Christopher M. Bishop - solution.pdf" "Pattern Recognition And Machine Learning - Christopher M. Bishop.pdf" 
+git commit -m "$1"
+git push github main
