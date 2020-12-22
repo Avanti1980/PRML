065f6c0aa45fac4864f1b1e809d7bab4a4ff205b
murongxixi <murongxixi@gmail.com>
2020-12-22 16:23:24 +0800

    update

--- upload.sh
+++ upload.sh
@@ -1,4 +1,4 @@
-git add 勘误.pdf 模式识别与机器学习.pdf 习题解答.pdf README.md solutions.tex upload.sh "Pattern Recognition And Machine Learning - Christopher M. Bishop - solution.pdf" "Pattern Recognition And Machine Learning - Christopher M. Bishop.pdf" 
+git add 勘误.pdf 模式识别与机器学习.pdf 习题解答.pdf README.md solutions.tex upload.sh "Pattern Recognition And Machine Learning - Christopher M. Bishop - solution.pdf" "Pattern Recognition And Machine Learning - Christopher M. Bishop.pdf" "Solution Manual For PRML.pdf"
 git commit -m "$1"
 git push github main
 git push gitee main
